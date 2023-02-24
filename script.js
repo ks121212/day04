57998c06a5ff94c40805065e7a70575621f03caa
ks121212 <124970377+ks121212@users.noreply.github.com>
2023-02-24 13:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+ //2.q
+   //for(var i=0;i<result.length;i++){
+     //  console.log(result[i].flag);
+    //}
+  
+    //3.q
+    for(var i=0;i<result.length;i++){
+        console.log(result[i].name,result[i].region,result[i].subregion,result[i].population);
+
+    }
+}
